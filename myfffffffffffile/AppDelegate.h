54c077ff6604fa9aed3e686ed06599cf65f2d600
wh <wh@whmac.local>
2016-03-23 20:09:35 +0800

    Initial Commit

---
+++ myfffffffffffile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myfffffffffffile
+//
+//  Created by wh on 16/3/23.
+//  Copyright © 2016年 wyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
